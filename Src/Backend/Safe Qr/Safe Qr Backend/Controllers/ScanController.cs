b269881515c5d4dff4153f7f3ec364a61b9c79db
Edwin <edwin.tgh1@gmail.com>
2026-06-09 19:40:36 +0800

    controller: added controller folder and skeleton code, program: added authentication cookie logic

---
+++ Src/Backend/Safe Qr/Safe Qr Backend/Controllers/ScanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Safe_Qr_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+    }
+}
